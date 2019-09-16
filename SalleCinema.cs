0c42c8c30d793f5acc064682db534b4476762d9c
Nadia Schwaller <nadia1.schwaller@gmail.com>
2019-09-16 10:19:35 +0200

    Classe sallecinema + contructeur + procedure et fonction nbplace et vendre placedisponible

---
+++ SalleCinema.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinéma
+{
+    class SalleCinema
+    {
+        private string nom;
+        private int nbplace;
+        private double tarifN;
+        private int NormalVendu;
+        private int ReduitVendu;
+
+        public SalleCinema(string lenom,int lenbplace, double letarifN)
+        {
+            this.nom = lenom;
+            this.nbplace = lenbplace;
+            this.tarifN = letarifN;
+            this.NormalVendu = 0;
+            this.ReduitVendu = 0;
+        }
+
+        public int nbPlacesDisponibles()
+        {
+            return (nbplace - (NormalVendu + ReduitVendu));
+        }
+        
+        public void vendrePlaces(int nbre,bool tarifReduit)
+        {
+            if ((this.nbPlacesDisponibles()-nbre) > 0)
+            {
+                double vendre;
+                if (tarifReduit)
+                {
+                    vendre = ((this.tarifN*nbre) * 0.80);
+                    MessageBox.show("Vous devez payer " + vendre);
+                    this.ReduitVendu += nbre;
+                }
+                else
+                {
+                    vendre = this.tarifN = nbre;
+                    MessageBox.show("Vous devez payer " + vendre);
+                    this.NormalVendu += nbre;
+                }
+            }
+            else
+            {
+                MessageBox.show("Il n'y a pas assez de place disponible pour vous" + vendre);
+            }
+    }
+}
